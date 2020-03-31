ed2af8773aa65d31b32b4b90ea88276cdb69c7b0
deadwingg <ivgomez505@gmail.com>
2020-03-31 00:46:35 -0300

    Changed to mySql for testing

---
+++ domain/target/classes/import.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO productos (descripcion, precio, codigo) VALUES ('pendrive', 650, 'PEN01');
+INSERT INTO productos (descripcion, precio, codigo) VALUES ('TV 43', 25000, 'TV01');
+INSERT INTO productos (descripcion, precio, codigo) VALUES ('PC INTEL', 65000, 'PC01');
+INSERT INTO productos (descripcion, precio, codigo) VALUES ('licuadora', 3000, 'EL01');
